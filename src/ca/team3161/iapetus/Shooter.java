d080da9cf718fd650cd6bee79a45a84e80f54bc9
Team 3161 <Team 3161@Programming>
2014-02-09 11:12:00 -0500

    Added Shooter class

---
+++ src/ca/team3161/iapetus/Shooter.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ca.team3161.iapetus;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+
+/**
+ *
+ * @author spok
+ * Class needs to control:  victor (winch) gauged by linear potentiometer (magnetopot), solenoid (super shifter for gearbox), 
+ *                          solenoid (for pickup), talon (roller), talon (fork)
+ * 
+ * Basic Overview
+ * Shooter:
+ *      Init: piston locked, motor cocked back
+ *      Auto: fork adjust, roller up, piston release, time delay, piston lock, winch motor activate
+ * Pickup Mode: fork piston down, roller down, roller motors activate
+ * Travel mode: roller motors off, roller down, fork up
+ * 
+ * TBD:
+ * Shooting modes (power and whatnot)
+ */
+
+public class Shooter {
+    
+    private final SpeedController winch = new Victor (7);
+    private final Solenoid trigger = new Solenoid (1);
+    private final Solenoid claw = new Solenoid (2);
+    private final SpeedController roller = new Talon (8);
+    private final SpeedController fork = new Talon (9);
+    private final Potentiometer drawback = new AnalogPotentiometer (10);
+    
+    public Shooter() {
+    }
+    
+    public void drawWinch(final double speed) {
+        if (getDrawback() >= 1.0d) {
+            return;
+        }
+        winch.set(normalize(speed));
+    }
+    
+    public void pullTrigger() {
+        trigger.set(true);
+    }
+    
+    public void resetTrigger() {
+        trigger.set(false);
+    }
+    
+    public void openClaw() {
+        claw.set(false);
+    }
+    
+    public void closeClaw() {
+        claw.set(true);
+    }
+    
+    /**
+     * @return whether the claw is closed or not.
+     */
+    public boolean getClaw() {
+        return claw.get();
+    }
+    
+    public void setRoller(final double speed) {
+        roller.set(normalize(speed));
+    }
+    
+    public void setFork(final double speed) {
+        fork.set(normalize(speed));
+    }
+    
+    /**
+     * @return a double from 0.0 to 1.0 representing % drawback of the shooter rod
+     */
+    public double getDrawback() {
+        return drawback.get();
+    }
+    
+    private static double normalize(final double val) {
+        if (val < -1.0d) return -1.0d;
+        if (val > 1.0d) return 1.0d;
+        return val;
+    }
+}
