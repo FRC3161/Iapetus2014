8fc617313f2e1fc24eb95e899c0fe87c0eac8774
Andrew Azores <andrew.azores@gmail.com>
2014-02-19 23:22:53 -0500

    fixed typo

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdpir -p doc/javadoc
+mkdir -p doc/javadoc
 
 command -v javadoc >/dev/null 2>&1|| { echo "javadoc is required but not found."; exit 1; }
 javadoc -d doc/javadoc -sourcepath src -subpackages ca.team3161 -source 1.4
