4ecbbc87c98f713fa6db71d7dcc7548dae9f1007
Andrew Azores <andrew.azores@gmail.com>
2014-05-01 00:16:01 -0400

    Moved Utils.round() to Math. Added Math.pow() implementations and Math.ln()

--- src/ca/team3161/lib/utils/Utils.java
+++ src/ca/team3161/lib/utils/Utils.java
@@ -47,36 +47,6 @@ public class Utils {
         return result;
     }
     
-    // 19 is the maximum digits a double can represent
-    private static final long[] TENS = new long[19];
-
-    static {
-        TENS[0] = 1;
-        for (int i = 1; i < TENS.length; i++) {
-            TENS[i] = 10 * TENS[i - 1];
-        }
-    }
-
-    /**
-     * @param val the double to round
-     * @param precision how many significant figures
-     * @return the rounded double
-     */
-    public static double round(final double val, int precision) {
-        if (precision < 0) {
-            precision = 0;
-        }
-        if (precision > 19) {
-            precision = 19;
-        }
-        final double unscaled = val * TENS[precision];
-        if (unscaled < Long.MIN_VALUE || unscaled > Long.MAX_VALUE) {
-            return val;
-        }
-        final long unscaledLong = (long) unscaled;
-        return (double) unscaledLong / TENS[precision];
-    }
-    
     /**
      * Normalize a PWM value so it remains in the range [-1.0, 1.0]
      * If a value above 1.0 is given, 1.0 is returned. If a value below -1.0
